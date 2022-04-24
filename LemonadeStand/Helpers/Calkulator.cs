93100eaa3100860e94c8f0ea0883c2c690c27173
Svante Joelsson <svante.joelsson@simployer.com>
2022-04-24 20:14:52 +0200

    En start

---
+++ LemonadeStand/Helpers/Calkulator.cs
@@ -0,0 +1,9 @@
+﻿namespace LemonadeStand.Helpers
+{
+    public static class Calkulator
+    {
+        public static int BuyLemonade(int x,int y) => x - y;
+
+        public static int LemonadesPrice(int Lemonades, int price) => Lemonades * price;
+    }
+}
---
+++ LemonadeStand/Models/IFruit.cs
@@ -0,0 +1,22 @@
+﻿namespace LemonadeStand.Models
+{
+    
+    public interface IFruit
+    {
+        string Name { get; }
+    }
+    public interface IRecipe
+    {
+        string Name { get; }
+        Type AllowedFruit { get; }
+        decimal ConsumptionPerGlass { get; }
+        int PricePerGlass { get; }
+    }
+    public interface IFruitPressService
+    {
+        FruitPressResult Produce(IRecipe recipe,
+        Collection<IFruit> fruits, int moneyPaid, int
+        orderedGlassQuantity);
+    }
+    
+}
